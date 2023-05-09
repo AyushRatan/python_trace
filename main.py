5b2840b1ef1330629bd133ef6ec5f5f69c11185d
AyushRatan <ayushratan.974@gmail.com>
2023-05-09 13:41:27 +0530

    UI code

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, render_template, redirect
+import pandas as pd
+
+df = pd.read_excel("output.xlsx")
+print(df)
+
+# Flask constructor
+app = Flask(__name__)
+
+# A decorator used to tell the application
+# which URL is associated function
+
+
+@app.route('/', methods = ["GET", "POST"])
+def form():
+	if request.method == "POST":
+		name = request.form["name"]
+		age = request.form["age"]
+		df.loc[len(df.index)] = [name,age]
+		with pd.ExcelWriter("output.xlsx", mode="a", if_sheet_exists="overlay") as writer:
+			df.to_excel(writer)
+	return render_template("home.html")
+
+
+if __name__ == '__main__':
+	app.run()
