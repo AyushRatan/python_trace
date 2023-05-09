dbea0226179ea064b1d6e4a176f40d8328713da1
AyushRatan <ayushratan.974@gmail.com>
2023-05-09 15:37:47 +0530

    updated UI

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+import os.path
+
 from flask import Flask, request, render_template, redirect
 import pandas as pd
+
+# filename = "output.xlsx"
+# if os.path.isfile(filename):
+# 	pass
+# else:
+# 	df = pd.DataFrame({"name":[],"age":[]})
+# 	with pd.ExcelWriter(filename) as writer:
+# 		df.to_excel(writer,index=False,sheet_name="sheet1")
 
 df = pd.read_excel("output.xlsx")
 print(df)
